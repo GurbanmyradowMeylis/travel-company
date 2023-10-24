6f39230eae758a8d1dd5cf4c768d8574998b4843
SuperACER <gurbanmyradowmeylis280@gmail.com>
2023-10-24 18:21:42 +0500

    did the first section and second

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
-  theme: {},
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-color": "#EAE8E8",
+        "secondary-color": "#1E1E1E",
+        "accent-color": "#C5C5C5",
+        nav_color: "#767679",
+      },
+      fontFamily: {
+        Lato_Black: ["Lato-Black", "cursive"],
+        Cardo_Bold: ["Cardo-Bold", "cursive"],
+        Lato_Regular: ["Lato-Regular", "cursive"],
+      },
+    },
+  },
   plugins: [],
 };
